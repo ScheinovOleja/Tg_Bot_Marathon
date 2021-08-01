76b377334fa139a6563d05596e384880d51e4e76
oleg <oleg20064@gmail.com>
2021-08-01 08:29:36 +0300

    Исправил рассылку сообщений

--- tg_marathon/marathon/views.py
+++ tg_marathon/marathon/views.py
@@ -1,9 +1,5 @@
-import json
-import os
 import time
-
 import requests
-from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
 
@@ -14,12 +10,15 @@ from .models import Config, User
 class SendMessageToTG(View):
 
     def post(self, request, *args, **kwargs):
-        text = f'Общее оповещение!!!\n\n\n{request.POST["message"]}\n\n\nНажмите /start, чтобы выйти в главное меню!'
+        text = f'{request.POST["message"]}\n\n\nНажмите /start, чтобы выйти в главное меню!'
         for user in User.objects.all():
-            url = f'https://api.telegram.org/bot{getattr(Config.objects.all().first(), "token_bot")}' \
-                  f'/sendMessage?chat_id={user.tg_id}&text={text}'
-            requests.get(url)
-            time.sleep(0.5)
+            try:
+                url = f'https://api.telegram.org/bot{getattr(Config.objects.all().first(), "token_bot")}' \
+                      f'/sendMessage?chat_id={user.tg_id}&text={text}'
+                requests.get(url)
+                time.sleep(0.5)
+            except Exception as exc:
+                continue
         return render(request, 'index.html', {'context': {'sent': True}})
 
     def get(self, request):
