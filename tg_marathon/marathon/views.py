e90c9cf5506988bccf43288636bafe821ee77c2c
oleg <oleg20064@gmail.com>
2021-08-01 08:49:12 +0300

    Сделал фоновую отправку сообщений через рассылку.

--- tg_marathon/marathon/views.py
+++ tg_marathon/marathon/views.py
@@ -1,3 +1,4 @@
+import threading
 import time
 import requests
 from django.shortcuts import render
@@ -9,18 +10,24 @@ from .models import Config, User
 # https://api.telegram.org/bot<token>/setWebhook?url=<url>/webhooks/tutorial/
 class SendMessageToTG(View):
 
-    def post(self, request, *args, **kwargs):
+    def mailing(self, request):
         text = f'{request.POST["message"]}\n\n\nНажмите /start, чтобы выйти в главное меню!'
+        i = 0
         for user in User.objects.all():
+            i += 1
             try:
                 url = f'https://api.telegram.org/bot{getattr(Config.objects.all().first(), "token_bot")}' \
                       f'/sendMessage?chat_id={user.tg_id}&text={text}'
                 requests.get(url)
-                time.sleep(0.5)
+                print(i)
             except Exception as exc:
+                print(i)
                 continue
+        print('Дошло всем')
+
+    def post(self, request, *args, **kwargs):
+        threading.Thread(target=self.mailing, args=(request,), daemon=True).start()
         return render(request, 'index.html', {'context': {'sent': True}})
 
     def get(self, request):
         return render(request, 'index.html', {'context': {'sent': False}})
-
